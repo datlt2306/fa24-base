0d4b7f9800b1b603c2d56991fc57937c3d9d542d
datlt2306 <datlt2306@gmail.com>
2024-11-29 09:56:37 +0700

    init

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,11 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-const queryClient = new QueryClient();
 createRoot(document.getElementById("root")).render(
-    <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </QueryClientProvider>
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
 );
